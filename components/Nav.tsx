c4e085ec08d5cb91a82b862944fbecb00d18dfc1
Posey <adeposi60@gmail.com>
2022-07-10 16:00:02 +0100

    Change banner image and nav layout

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 function Nav() {
     return (
         <header>
-            <nav className='container flex flex-col items-center gap-4 py-8'>
-                <div className='flex flex-col text-center gap-0'>
-                    <span className='font-bold text-xl'>CRYSTAL</span>
-                    <span className='ml-24 font-medium italic'>HAVEN</span>
+            <nav className='container flex justify-between items-center gap-4 py-8'>
+                <div className='flex text-center gap-0'>
+                    <span className='font-bold text-xl'>DE CRYSTAL HAVEN</span>
+                    {/* <span className='ml-24 font-medium italic'>HAVEN</span> */}
                 </div>
                 <div className='flex gap-x-8 font-light'>
                         <Link href="/">
